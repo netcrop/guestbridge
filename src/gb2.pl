2991a56391e09997a536a3923892b8b620cd4fa9
Yang Li <git@netcrop.org>
2020-06-07 11:10:47 +0000

    2020-06-07 19:10:47

--- src/gb2.pl
+++ src/gb2.pl
@@ -6,6 +6,14 @@ use POSIX qw(setsid setgid setuid);
 use Data::Dumper;
 die "[guestimage file]: $!" unless defined $ARGV[0] && -r $ARGV[0]; 
 my %me = ();
+sub clean {
+    return unless me.cleaup;
+    me.cleanup = 0;
+}
+sub delocate {
+    clean();
+    die @_;
+}
 sub setup {
     me.guestimg = $ARGV[0];
     $_ = me.guestimg;
@@ -32,17 +40,58 @@ sub setup {
     me.qemu = qw QEMU;
     me.bridge = qw BRIDGE;
     me.virtiofsd = qw VIRTIOFSD;
+    me.mv = qw MV;
     me.guestcfg = "me.gbdir/conf/me.guestname";
+    delocate "Pls add: me.User[0] to grp: me.Kvm[0]" unless me.User[2] != me.Kvm[2];
+    delocate "me.guestcfg not avaliable." unless -r me.guestcfg;
+    delocate "me.socksdir/me.guestname still in place" if -S "me.socksdir/me.guestname";
+    if (not -w me.vfiodir or not -x me.vfiodir ){
+        run("@me.Permit me.chown :me.Kvm[3] me.vfiodir");
+        run("@me.Permit me.chmod 0775 me.vfiodir");
+    }
+    if(! -d me.virtiofsdsocksdir || ! -w me.virtiofsdsocksdir || ! -x -w me.virtiofsdsocksdir){
+        me.tmp = int(rand(99999)) + 100000;
+        me.tmp = "var/tmp/me.tmp";
+        mkdir(me.tmp, 0770);
+        chmod (me.tmp, 0770);
+        chown (me.User[2], me.Kvm[2], me.tmp);
+    }
 }
-sub clean {
-    return unless me.cleaup;
-    me.cleanup = 0;
-}
-sub delocate {
-    clean();
-    die @_;
+sub config {
+    open(INPUT, '<', me.guestcfg) or delocate "can't open me.guestcfg";
+    chomp(@me.Config = <INPUT>);
+    foreach(@me.Config){
+        %me.Tmp = ();
+        foreach(split(/,/)){
+            if(m;([^"' ]+)\s*=\s*["']{0,1}([^"' ]+)["']{0,1};){
+                me.Tmp{$1} = $2;
+                next;
+            }
+            if(m;([^"' ]+)\s*["']{0,1}([^"' ]+)["']{0,1};){
+                me.Tmp{$1} = $2;
+                next;
+            }
+        }
+        if ( defined me.Tmp{mac} && defined me.Tmp{netdev} ){
+            me.Tap{me.Tmp{netdev}} = me.Tmp{mac}; 
+            me.tmp = me.Tmp{mac} =~ tr/://rd;
+            me.Bridge{me.tmp} = me.Tmp{mac};
+            next;
+        }
+        if ( defined me.Tmp{path} ){
+            me.Tmp{path} =~ m;me.guestname-(.+)\.sock;;
+            me.Path{$1} = $1 =~ tr;@;\/;r;
+            next;
+        }
+        if (defined me.Tmp{-device} && defined me.Tmp{host}){
+            me.Wish{me.Tmp{host}} = me.Tmp{-device} if me.Tmp{host} =~ me.bdfpattern;
+            next;
+        }
+    }
 }
 sub run {
     system( split( /\s+/, $_[0])) == 0 or delocate "$_[0]: $!";
 }
 setup();
+config();
+print Dumper(\%me);
